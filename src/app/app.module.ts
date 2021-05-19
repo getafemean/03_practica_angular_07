688ba16f8f8e4591de18529cbdc4b30762ce6b9d
getafemean <getafemean@gmail.com>
2021-05-19 17:27:51 +0200

    crear cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { CrearClienteComponent } from './ventas/crear-cliente/crear-cliente.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ventas/crear-cliente/crear-cliente.component.ts
+++ src/app/ventas/crear-cliente/crear-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClientesService } from 'src/app/servicios/clientes.service';
 
 @Component({
   selector: 'app-crear-cliente',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearClienteComponent implements OnInit {
 
-  constructor() { }
+  cliente: any = {
+    nombre: '',
+    cif: '',
+    localidad: ''
+  }
+
+  constructor(private clientesService: ClientesService, 
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  addCliente() {
+    this.clientesService.setCliente(this.cliente);
+    this.router.navigate(['/ventas/listado-clientes']);
   }
 
 }
